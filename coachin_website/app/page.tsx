0708388ee60b0df94e0ca83f4bfebb27f3dfab5c
VVinaykumar003 <vvinaykumar2000@gmail.com>
2026-05-13 19:01:08 +0530

    home

---
+++ coachin_website/app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Home from './(user)/page'
+
+const page = () => {
+  return (
+    <div>
+    <h1 className='text-3xl font-bold text-center mt-10'>Welcome to the Home Page</h1>
+      
+    </div>
+  )
+}
+
+export default page
