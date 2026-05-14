352ada652a4c19673f031aa9e3d8ddb6b839e740
VVinaykumar003 <vvinaykumar2000@gmail.com>
2026-05-14 10:06:05 +0530

    checking

---
+++ coachin_website/app/page.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <div className="card bg-base-content shadow-sm border border-base-200 mb-6">
+        <div className="card-body p-4 sm:p-6">
+          <h2 className="card-title text-lg mb-2">Recent Registrations</h2>
+          <div className="overflow-x-auto"> 
+
+      
+    </div>
+    </div>
+    </div>
+    </div>
+  )
+}
+
+export default Home
