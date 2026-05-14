5bb7ebdeb245c71bb06a0d6009b7f76ebafa8fe9
VVinaykumar003 <vvinaykumar2000@gmail.com>
2026-05-14 10:49:01 +0530

    new changes

--- coachin_website/app/layout.tsx
+++ coachin_website/app/layout.tsx
@@ -10,7 +10,7 @@ export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
-}>) {
+}>) { 
   return (
     <html lang="en" className={`h-full antialiased`}>
       <body>{children}</body>
